b6c4c6ee42cd4f09baf59f89afe9e4af1634da8a
eladinbar <62742789+eladinbar@users.noreply.github.com>
2023-02-24 02:06:45 +0200

    Update SettingsMenu.cs
    Moved initialToggle to Awake so that initialization can occur during Start.

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -8,21 +8,19 @@ using UnityEngine.UI;
 public class SettingsMenu : MonoBehaviour {
     [SerializeField] AudioMixer audioMixer;
     private Toggle fullscreenToggle;
-    
-    #if UNITY_EDITOR
+
+    public Toggle FullscreenToggle => fullscreenToggle;
+
+#if UNITY_EDITOR
     private EditorWindow window;
     private bool initialToggle = true;
     #endif
 
     private void Awake() {
         fullscreenToggle = FindObjectOfType<Toggle>();
+        
         #if UNITY_EDITOR
         window = EditorWindow.focusedWindow;
-        #endif
-    }
-
-    private void Start() {
-        #if UNITY_EDITOR
         fullscreenToggle.isOn = window.maximized;
         initialToggle = false;
         #endif
@@ -36,12 +34,13 @@ public class SettingsMenu : MonoBehaviour {
         QualitySettings.SetQualityLevel(qualityLevel);
     }
 
-    public void FullscreenToggle(bool toFullscreen) {
+    public void ToggleFullscreen(bool toFullscreen) {
         #if UNITY_EDITOR
             if (!initialToggle)
                 window.maximized = !window.maximized;
         #else
             Screen.fullScreen = toFullscreen;
         #endif
+        
     }
 }
