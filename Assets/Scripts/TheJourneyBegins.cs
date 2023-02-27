4f62d5a8c53b71f7a79c6d81d6caab83d461af64
eladinbar <eladin@post.bgu.ac.il>
2023-02-27 22:26:58 +0200

    Update TheJourneyBegins.cs
    Added a small animation for players to understand they're interacting with the player.
    Added image support and press space to continue text

--- Assets/Scripts/TheJourneyBegins.cs
+++ Assets/Scripts/TheJourneyBegins.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class TheJourneyBegins : MonoBehaviour {
     private static readonly int IsJumping = Animator.StringToHash("isJumping");
+    private static readonly int Punching = Animator.StringToHash("Punch");
+    private static readonly int Kicking = Animator.StringToHash("Kick");
+    
     private PlayerInput playerInput;
     private Animator playerAnimator;
+    private Rigidbody2D playerRigidbody;
     
     private bool finishedDisplaying;
     int currentIndex = 0;
@@ -16,20 +21,24 @@ public class TheJourneyBegins : MonoBehaviour {
     private int lastInput = 0;
     private int currentInput = 0;
     int keyPressCount = 0;
-    [SerializeField] int keyPressesRequired = 3;
-
+    [SerializeField] int keyPressesRequired = 2;
     
     [SerializeField] List<TextMeshProUGUI> textList;
+    [SerializeField] Image spaceImage;
+    [SerializeField] TextMeshProUGUI continueText;
     [SerializeField] TextMeshProUGUI tutorialText;
+    [SerializeField] List<Image> tutorialImages;
 
     void Start() {
         playerInput = PlayerMechanics.Instance.GetComponent<PlayerInput>();
         playerAnimator = PlayerMechanics.Instance.GetComponent<Animator>();
+        playerRigidbody = PlayerMechanics.Instance.GetComponent<Rigidbody2D>();
         // Initialize all text elements
-        foreach (TextMeshProUGUI text in textList) {
+        foreach (TextMeshProUGUI text in textList)
             text.gameObject.SetActive(false);
-        }
         tutorialText.gameObject.SetActive(false);
+        foreach (Image image in tutorialImages)
+            image.gameObject.SetActive(false);
         // Fade in from black
         // Display initial capsule canvas
         ShowNextText();
@@ -40,28 +49,41 @@ public class TheJourneyBegins : MonoBehaviour {
         // Ensure all other start functions have finished executing
         yield return new WaitForSeconds(Mathf.Epsilon);
         // Revert initial animation sprite to 'Idle'
-        PlayerMechanics.Instance.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-        PlayerMechanics.Instance.GetComponent<Animator>().SetBool(IsJumping, false);
-        // Stop time and input
-        Time.timeScale = 0f;
+        playerRigidbody.velocity = Vector2.zero;
+        playerAnimator.SetBool(IsJumping, false);
+        // Stop physics and input
+        PlayerMechanics.Instance.moveEnabled = false;
+        PlayerMechanics.Instance.jumpEnabled = false;
+        playerRigidbody.isKinematic = true;
         playerInput.DeactivateInput();
     }
 
     private void Update() {
         if (Input.GetKeyDown(KeyCode.Space) && !finishedDisplaying) {
+            playerRigidbody.velocity = Vector2.zero;
             ShowNextText();
         }
 
         else if (finishedDisplaying && keyPressCount < keyPressesRequired) {
+            // Reactivate input for animation
+            playerInput.ActivateInput();
+            playerRigidbody.velocity = Vector2.zero;
+            
+            int animatorState = keyPressCount == 0 ? Punching : Kicking;
+
             // Check for alternating left/right key presses
             if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) {
                 // Handle left arrow key press
                 currentInput = -1;
+                PlayerMechanics.Instance.transform.localScale = new Vector3(-1, 1 ,1);
+                playerAnimator.SetTrigger(animatorState);
             }
 
             if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) {
                 // Handle right arrow key press
                 currentInput = 1;
+                PlayerMechanics.Instance.transform.localScale = Vector3.one;
+                playerAnimator.SetTrigger(animatorState);
             }
 
             // Check for alternating key presses
@@ -75,9 +97,9 @@ public class TheJourneyBegins : MonoBehaviour {
 
         if (finishedDisplaying && keyPressCount >= keyPressesRequired) {
             // Wait for left right input
-            PlayerMechanics.Instance.GetComponent<Rigidbody2D>().isKinematic = false;
-            Time.timeScale = 1f;
-            playerInput.ActivateInput();
+            playerRigidbody.isKinematic = false;
+            PlayerMechanics.Instance.moveEnabled = true;
+            PlayerMechanics.Instance.jumpEnabled = true;
             Destroy(this.gameObject);
         }
     }
@@ -85,8 +107,12 @@ public class TheJourneyBegins : MonoBehaviour {
     private void ShowNextText() {
         if (currentIndex >= textList.Count) {
             textList[currentIndex - 1].gameObject.SetActive(false);
+            spaceImage.gameObject.SetActive(false);
+            continueText.gameObject.SetActive(false);
             if(tutorialText)
                 tutorialText.gameObject.SetActive(true);
+            foreach (Image image in tutorialImages)
+                image.gameObject.SetActive(true);
             finishedDisplaying = true;
         }
         else {
