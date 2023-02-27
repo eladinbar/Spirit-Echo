d8732a0105d2d5a70114386785c9017bf635ce1c
RoyHamamy <royha@post.bgu.ac.il>
2023-02-27 23:22:40 +0200

    recently added changes

---
+++ Assets/CyborgInteraction.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CyborgInteraction : MonoBehaviour
+{
+
+   void Start()
+    {
+        PlayerMechanics.Instance.unlockedGravityShift = true;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
